17a5af653dd20689c66d4ce1255d575a5183957a
OpaL <opal@palojarvi.fi>
2018-03-07 21:55:02 +0200

    Fix config reader and ip address parser

--- src/main.py
+++ src/main.py
@@ -4,17 +4,20 @@
 import asyncio
 import json
 import ipaddress
+import os
+import sys
 
 import artnet
 
 def load_config():
-    with open('../config/config.json', 'r') as f:
+    pathname = os.path.dirname(sys.argv[0]) 
+    with open(os.path.abspath(pathname) + '/../config/config.json', 'r') as f:
         config = json.load(f)
         f.close()
         return config
 
 def get_broadcast_address(config):
-    return ipaddress.IPv4Network("%s/%s" % (config["networking"]["address"], config["networking"]["subnet"])).broadcast_address
+    return str(ipaddress.ip_interface("%s/%s" % (config["networking"]["address"], config["networking"]["subnet"])).network.broadcast_address)
 
 def pretty_print(packet):
     print("Value change detected: %s" % channel)
