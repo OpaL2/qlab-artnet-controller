6a8d925585cdff2df48937e9de17cde6b9aee810
OpaL <opal@palojarvi.fi>
2018-03-08 18:14:32 +0200

    Remove unused exception part

--- src/main.py
+++ src/main.py
@@ -23,7 +23,6 @@ def pretty_print(packet):
     print("Value change detected" )
     print("New value buffer: \n %s" % packet)
 
-
 working_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 config = load_config(working_dir)
 
@@ -32,24 +31,20 @@ qlab = qlab.QLab(config["DMX512"]["control_channel"], config["DMX512"]["paramete
 
 loop = asyncio.get_event_loop()
 addr = (get_broadcast_address(config), config["networking"]["port"])
+
 ArtNet = artnet.ArtNet(loop, addr)
 worker = ArtNet.create_universe(config["DMX512"]["universe"]) \
     .create_callback_executor() \
     .add_coroutine_callback(*qlab.get_callback()) \
     .add_callback(2, pretty_print)
 
+    asyncio.ensure_future(worker.run())
+
 # Running loop, exiting on keyboard interrupt
 try:
-    asyncio.ensure_future(worker.run())
     loop.run_forever()
 except KeyboardInterrupt:
     worker.close()
     ArtNet.close()
     loop.close()
     exit(0)
-except Exception as e:
-    print("Encountered error: %s, exiting..." % e)
-    worker.close()
-    ArtNet.close()
-    loop.close()
-    exit(1)
